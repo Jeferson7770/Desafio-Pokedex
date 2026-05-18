1959a1e92331549da17048191bd37b1aa9539296
Atilio Hector <thehecktour@Atilios-MacBook-Air.local>
2026-05-18 20:16:43 -0300

    feat: add logout endpoint and psycopg lib

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from fincecore.src.users.views.logout import LogoutView
+
 from ..users.views.laywer import LawyerProfileView
 from .views.register import (
     RegisterView,
@@ -17,6 +19,7 @@ from .views.reset_password import (
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
+    path('logout/', LogoutView.as_view()),
     path('laywer-profile/', LawyerProfileView.as_view()),
     path('password-reset/', RequestPasswordResetView.as_view()),
     path('password-reset-confirm/', ConfirmPasswordResetView.as_view()),
