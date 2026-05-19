3b14b03919f9690da63b6a0c08664be0f6be3b09
Atilio Hector <thehecktour@Atilios-MacBook-Air.local>
2026-05-19 19:53:47 -0300

    fix: remove url from wrong place

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 
-from fincecore.src.expenses.models.expenses import Expense
-
 from ..users.views.laywer import LawyerProfileView
 from .views.register import (
     RegisterView,
@@ -24,7 +22,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
     path('logout/', LogoutView.as_view()),
-    path('expenses/', Expense.as_view()),
     path('laywer-profile/', LawyerProfileView.as_view()),
     path('password-reset/', RequestPasswordResetView.as_view()),
     path('password-reset-confirm/', ConfirmPasswordResetView.as_view()),
