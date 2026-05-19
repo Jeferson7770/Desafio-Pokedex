7ac04a4999b03a95a29d47722af0517921fcf381
Atilio Hector <thehecktour@Atilios-MacBook-Air.local>
2026-05-19 19:50:18 -0300

    feat: add expense url

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from fincecore.src.expenses.models.expenses import Expense
+
 from ..users.views.laywer import LawyerProfileView
 from .views.register import (
     RegisterView,
@@ -22,6 +24,7 @@ urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
     path('logout/', LogoutView.as_view()),
+    path('expenses/', Expense.as_view()),
     path('laywer-profile/', LawyerProfileView.as_view()),
     path('password-reset/', RequestPasswordResetView.as_view()),
     path('password-reset-confirm/', ConfirmPasswordResetView.as_view()),
