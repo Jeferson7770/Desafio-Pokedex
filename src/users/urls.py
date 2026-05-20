ac6e8f647d04ec9b5a2b9cc0d760296a2cdd7198
Atilio Hector <thehecktour@Atilios-MacBook-Air.local>
2026-05-20 19:17:30 -0300

    fix(users): mapeia metodos HTTP explicitamente para o LawyerProfileViewSet

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,28 +1,24 @@
 from django.urls import path
 
 from ..users.views.laywer import LawyerProfileViewSet
-from .views.register import (
-    RegisterView,
-)
-
-from .views.login import (
-    LoginView
-)
-
-from .views.logout import (
-    LogoutView
-)
-
-from .views.reset_password import (
-    RequestPasswordResetView,
-    ConfirmPasswordResetView
-)
+from .views.register import RegisterView
+from .views.login import LoginView
+from .views.logout import LogoutView
+from .views.reset_password import RequestPasswordResetView, ConfirmPasswordResetView
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
     path('logout/', LogoutView.as_view()),
-    path('laywer-profile/', LawyerProfileViewSet.as_view()),
+    
+    path('laywer-profile/', LawyerProfileViewSet.as_view({
+        'get': 'list',
+        'post': 'create',
+        'put': 'update',
+        'patch': 'partial_update',
+        'delete': 'destroy'
+    })),
+    
     path('password-reset/', RequestPasswordResetView.as_view()),
     path('password-reset-confirm/', ConfirmPasswordResetView.as_view()),
 ]
