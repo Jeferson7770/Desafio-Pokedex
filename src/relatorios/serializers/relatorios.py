34858519caf46c522c816d6fcc0a19b47e110ecf
Atilio Hector <thehecktour@Atilios-MacBook-Air.local>
2026-05-23 10:15:57 -0300

    fix: change serialier to modelserializer on financialreportdashboardserializer

--- src/relatorios/serializers/relatorios.py
+++ src/relatorios/serializers/relatorios.py
@@ -1,14 +1,14 @@
 from rest_framework import serializers
 from ..services.cashflow import CashFlowEngine
 
-class FinancialReportDashboardSerializer(serializers.Serializer):
+class FinancialReportDashboardSerializer(serializers.ModelSerializer):
     """
     Serializer leve que consome as estruturas de dados validadas pelo
     CashFlowEngine, garantindo velocidade de entrega para o Front-end.
     """
     
-    def to_representation(self, queryset):
-        engine = CashFlowEngine(queryset)
+    def to_representation(self, instance):
+        engine = CashFlowEngine(instance)
         report_payload = engine.generate_full_report()
         
         return report_payload.to_dict()
