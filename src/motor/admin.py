482572faf5cfaec7458b3c64fd8cdb37028ac5e2
Atilio Hector <thehecktour@Atilios-MacBook-Air.local>
2026-05-28 20:18:09 -0300

    refactor(motor): altera fluxo para cálculo dinâmico no GET raiz e POST seco para salvar o mês vigente

--- src/motor/admin.py
+++ src/motor/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from .models import MotorPrioridade
+from .models import SimulacaoPrioridade
 
-@admin.register(MotorPrioridade)
-class MotorPrioridadeAdmin(admin.ModelAdmin):
-    list_display = ("id", "prioridade", "created_at", "updated_at")
+@admin.register(SimulacaoPrioridade)
+class SimulacaoPrioridadeAdmin(admin.ModelAdmin):
+    list_display = ("id", "reference_date", "saldo_total_disponivel", "saldo_restante_pos_pagamentos", "created_at", "updated_at")
     list_filter = ("created_at", "updated_at")
-    search_fields = ("prioridade",)
+    search_fields = ("reference_date",)
