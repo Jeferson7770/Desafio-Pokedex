03b80be0dedb5a25a68c7f4911e840023ce72022
Atilio Hector <thehecktour@Atilios-MacBook-Air.local>
2026-06-01 09:56:02 -0300

    fix(infra): adiciona defaults para variaveis do posthog evitando quebra no ci

--- src/users/apps.py
+++ src/users/apps.py
@@ -8,8 +8,8 @@ class UsersConfig(AppConfig):
     name = "src.users"
 
     def ready(self):
-        api_key = config("POSTHOG_API_KEY")
-        host = config("POSTHOG_HOST")
+        api_key = config("POSTHOG_API_KEY", default=None)
+        host = config("POSTHOG_HOST", default="https://us.i.posthog.com")
 
         if api_key:
             posthog.api_key = api_key
