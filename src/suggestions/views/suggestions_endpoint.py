6f4abd589b182b9b487a4df925d50ed6f0a167f8
Atilio Hector <thehecktour@Atilios-MacBook-Air.local>
2026-06-01 10:40:51 -0300

    feat(telemetry): adiciona rastreamento no envio de feedbacks e sugestoes dos usuarios

--- src/suggestions/views/suggestions_endpoint.py
+++ src/suggestions/views/suggestions_endpoint.py
@@ -1,7 +1,10 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
 from rest_framework.exceptions import PermissionDenied, ValidationError
 from ..models.suggestions_structure import Suggestion
 from ..serializers.suggestions import SuggestionSerializer
+from ...users.utils.telemetry import track_event
+
 
 class SuggestionViewSet(viewsets.ModelViewSet):
     serializer_class = SuggestionSerializer
@@ -20,6 +23,23 @@ class SuggestionViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         firm = self._get_user_firm(self.request.user)
         return Suggestion.objects.filter(firm=firm).order_by("-created_at")
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        track_event(
+            user=request.user,
+            event_name="sugestao_enviada_sucesso",
+            properties={
+                "suggestion_id": serializer.data.get("id"),
+                "categoria": serializer.data.get("category", "N/A")
+            }
+        )
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     def perform_create(self, serializer):
         if not self.request.user.is_authenticated:
