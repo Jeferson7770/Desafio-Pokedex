c39d2fe4ca08c287c20181da0c0277df1a937393
Atilio Hector <thehecktour@Atilios-MacBook-Air.local>
2026-06-01 20:24:56 -0300

    fix(finance): altera filtro de honorarios para buscar por vencimento de parcelas ativas

--- src/honorarios/views/honorarios.py
+++ src/honorarios/views/honorarios.py
@@ -1,7 +1,8 @@
+from django.db.models import Prefetch
 from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
 from rest_framework.exceptions import PermissionDenied, ValidationError
-from ..models.honorarios import Honorario
+from ..models.honorarios import Honorario, ParcelaHonorario
 from ..serializers.honorarios import HonorarioSerializer
 from ...users.utils.telemetry import track_event
 
@@ -17,19 +18,22 @@ class HonorarioViewSet(viewsets.ModelViewSet):
         return membership.firm
 
     def get_queryset(self):
-        queryset = Honorario.objects.filter(
-            firm__members__user=self.request.user
-        ).prefetch_related('installments')
+        user = self.request.user
+        firm = self._get_user_firm(user)
+        
+        queryset = Honorario.objects.filter(firm=firm)
 
         year = self.request.query_params.get("year")
         month = self.request.query_params.get("month")
 
+        installment_filters = {}
+
         if year:
             try:
-                queryset = queryset.filter(date__year=int(year))
+                installment_filters["due_date__year"] = int(year)
             except ValueError:
                 track_event(
-                    user=self.request.user,
+                    user=user,
                     event_name="honorarios_filtro_erro",
                     properties={"year_tentado": year, "motivo": "ano_invalido"}
                 )
@@ -37,27 +41,34 @@ class HonorarioViewSet(viewsets.ModelViewSet):
 
         if month:
             try:
-                queryset = queryset.filter(date__month=int(month))
+                installment_filters["due_date__month"] = int(month)
             except ValueError:
                 track_event(
-                    user=self.request.user,
+                    user=user,
                     event_name="honorarios_filtro_erro",
                     properties={"month_tentado": month, "motivo": "mes_invalido"}
                 )
                 raise ValidationError("O parâmetro 'month' deve ser um número inteiro válido.")
 
+        if installment_filters:
+            queryset = queryset.filter(installments__due_date__year=int(year) if year else queryset,
+                                       installments__due_date__month=int(month) if month else queryset).distinct()
+            
+            queryset = queryset.prefetch_related(
+                Prefetch(
+                    "installments",
+                    queryset=ParcelaHonorario.objects.filter(**installment_filters)
+                )
+            )
+        else:
+            queryset = queryset.prefetch_related('installments')
+
         return queryset
 
     def list(self, request, *args, **kwargs):
-        year = request.query_params.get("year")
-        month = request.query_params.get("month")
-
-        if year or month:
-            queryset = self.filter_queryset(self.get_queryset())
-            serializer = self.get_serializer(queryset, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
-        return super().list(request, *args, **kwargs)
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
